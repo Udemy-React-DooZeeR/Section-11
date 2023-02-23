d26e37071df16dfb3b65891054c1e8a8eaacad72
DooZeeR <zsolt.ordas@gmail.com>
2023-02-23 09:20:18 +0100

    Cart Modal ready

--- food_order_app/src/components/Layout/Header.js
+++ food_order_app/src/components/Layout/Header.js
@@ -3,12 +3,12 @@ import classes from './Header.module.css';
 import HeaderCartButton from './HeaderCartButton';
 import MealsImage from '../../assets/meals.jpg';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <React.Fragment>
         <header className={classes.header}>
             <h1>ReactMeals</h1>
-            <HeaderCartButton/>
+            <HeaderCartButton onClick={props.onCartShow}/>
         </header>
         <div className={classes['main-image']}>
             <img src={MealsImage} alt='A table full of delicious food!'/>
